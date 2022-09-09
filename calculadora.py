771ac308bc59997d6e841019e6ea88f0ab6f3831
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-09-09 18:11:18 -0300

    tercer commit, deleteado el primer archivo, e ingresado el que utilizaremos, ademas de establecida la ventana principal, con sus respectivos botones

---
+++ calculadora.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, Label, Button
+
+class calculadora:
+    def __init__(self, master):
+        self.master = master
+        master.title("CALCULATOR 3000")
+        self.etiqueta = Label(master, text="Esta es la primera ventana!")
+        self.etiqueta.pack()
+        self.botonSumar = Button(master, text="Sumar", command=self.sumar)
+        self.botonSumar.pack()
+        self.botonRestar = Button(master, text="Restar", command=self.restar)
+        self.botonRestar.pack()
+        self.botonMultiplicar = Button(master, text="Multiplicar", command=self.multiplicar)
+        self.botonMultiplicar.pack()
+        self.botonDividir = Button(master, text="Dividir", command=self.dividir)
+        self.botonDividir.pack()
+        self.botonCerrar = Button(master, text="Cerrar", command=master.quit)
+        self.botonCerrar.pack()
+
+    def sumar(valor1,valor2):
+        print(valor1 + valor2)
+    
+    def restar(valor1,valor2):
+        print(valor1 - valor2)
+    
+    def multiplicar(valor1,valor2):
+        print(valor1 * valor2)
+    
+    def dividir(valor1,valor2):
+        print(valor1 / valor2)
+    
+root = Tk()
+miVentana = calculadora(root)
+root.mainloop()
