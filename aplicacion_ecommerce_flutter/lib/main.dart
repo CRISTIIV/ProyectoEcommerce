a595bdc0c62a01df94d4e6894eb66496fa2f9dc4
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-11-04 19:51:06 -0300

    Update main.dart

--- aplicacion_ecommerce_flutter/lib/main.dart
+++ aplicacion_ecommerce_flutter/lib/main.dart
@@ -1,67 +1,20 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiApp());
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key key}) : super(key: key);
 
-  Widget build(BuildContext context){
+class MiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Probando Flutter -MyApp-',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      //home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget{
-  MyHomePage({Key key}) : super(key: key);
-
-  //final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-  //int _counter = 0;
-  /*
-  void _incrementCounter(){
-    setState(() {
-      _counter++;
-    });
-  }*/
-
-  @override
-  Widget build(BuildContext context){
-    return Container(
-      child: child,
-    );
-    /*return Scaffold(
-      appBar: AppBar(
-        title: Text('Probando Flutter -MyHomePage-'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Has pulsado el botón esta cantidad de veces:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      title: 'Aplicación base',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Barra de la aplicación'),
         ),
+        body: Container(
+          child: Image.network(''),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );*/
+    );
   }
 }
