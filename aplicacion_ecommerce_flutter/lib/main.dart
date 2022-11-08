5e22b15667086d881046b725fd26d74b58684985
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-11-07 22:33:51 -0300

    Update main.dart

--- aplicacion_ecommerce_flutter/lib/main.dart
+++ aplicacion_ecommerce_flutter/lib/main.dart
@@ -2,25 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MiApp());
 
-
 class MiApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aplicación base',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Barra de la aplicación'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Container(
-              child: Image.network('https://raw.githubusercontent.com/CRISTIIV/ProyectoEcommerce/master/PantallaINICIO%20PNG%20(mod).png'),
+        title: 'Ecommerce',
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'NEKO STORE',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 0, 0),
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w900),
             ),
-          ],
-        ),
-    ));
+            backgroundColor: Color.fromARGB(255, 0, 0, 0),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                child: Image.network(
+                    'https://raw.githubusercontent.com/CRISTIIV/ProyectoEcommerce/master/PantallaINICIO%20PNG%20(modu).png'),
+              ),
+            ],
+          ),
+        ));
   }
 }
