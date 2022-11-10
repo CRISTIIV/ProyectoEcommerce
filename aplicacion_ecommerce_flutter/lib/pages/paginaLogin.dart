48219cad7dc4bfa308cf0b2e8e425bc48938e217
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-11-09 22:53:32 -0300

    Update paginaLogin.dart

--- aplicacion_ecommerce_flutter/lib/pages/paginaLogin.dart
+++ aplicacion_ecommerce_flutter/lib/pages/paginaLogin.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(Login());
+
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
