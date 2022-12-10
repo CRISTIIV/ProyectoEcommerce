f4ee9c560afbf24658f31f553e7438a43a41084e
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-12-10 18:45:43 -0300

    Update main.dart

--- aplicacion_ecommerce_flutter/lib/main.dart
+++ aplicacion_ecommerce_flutter/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ecommerce_app/pages/signin_screen.dart';
+import 'package:aplicacion_ecommerce_flutter/pages/signin_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
