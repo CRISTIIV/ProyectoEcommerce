3454400b4e7e53e2bfb83b0e00ef32aa8fbe6082
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-12-10 18:41:38 -0300

    Create home_screen.dart

---
+++ aplicacion_ecommerce_flutter/lib/pages/home_screen.dart
@@ -0,0 +1,519 @@
+import 'package:ecommerce_app/pages/signin_screen.dart';
+import 'package:ecommerce_app/utils/color_utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  @override
+  Widget build(BuildContext context){
+    
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            hexStringToColor("5E61F4"),
+            hexStringToColor("9546C4"),
+            Colors.black
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+            child: Padding(padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                optionsBar(),
+                categories()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row optionsBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        ElevatedButton(
+          child: Text("Configurar cuenta"),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.red,
+            onPrimary: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(32.0),
+            ),
+          ),
+          onPressed: () {
+            FirebaseAuth.instance.signOut().then((value) {
+              print("Ventana de configuración");
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SignInScreen()));//UserConfig()));
+            });
+          },
+        ),
+        Row(
+          children: <Widget>[
+            ElevatedButton(
+                  child: Text("Cerrar sesión"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 43, 0, 255),
+                    onPrimary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0),
+                    ),
+                  ),
+
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut().then((value) {
+                      print("Sesión cerrada");
+                      Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const SignInScreen()));
+                    });
+                  },
+                ),
+            IconButton(
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                //AÑADIR BUSCADOR DE PRODUCTOS CON FIREBASE
+                print("Buscador de productos"); 
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//SearchScreen()));
+              },
+            ),
+            IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                //VER CARRITO DE COMPRAS
+                print("Carrito de compras");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//CartScreen()));
+              },
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Column categories() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.woman,
+                color: Colors.white,
+              ),
+              onPressed: (){
+                print("Mujeres");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//WomenScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+                "Mujeres",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.man,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Hombres");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//MenScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Hombres",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.snowshoeing,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Calzado");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//ShoeScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Calzado",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.pregnant_woman,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Bebés");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//BabyScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Bebés",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.piano,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Instrumentos musicales");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//InstrumentScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Instrumentos",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.movie,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Peliculas");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//MovieScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Películas",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.gamepad,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("VideoJuegos");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//GameScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("VideoJuegos",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.face,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Belleza");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//BeautyScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Belleza",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.camera,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Cámaras");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//CameraScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Cámaras",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.book,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Libros");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//BookScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Libros",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.pets,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                print("Mascotas");
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SignInScreen()));//PetScreen()));
+              }),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text("Mascotas",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+}
