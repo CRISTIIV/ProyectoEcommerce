0353e098765edc4e4c0993677bb51c894e9b64f0
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-12-11 03:21:04 -0300

    Update paginaUser.dart

--- aplicacion_ecommerce_flutter/lib/pages/paginaUser.dart
+++ aplicacion_ecommerce_flutter/lib/pages/paginaUser.dart
@@ -1,2 +1,46 @@
-import 'package:flutter/cupertino.dart';
+import 'package:ecommerce_app/reusable_widgets/reusable_widget.dart';
+import 'package:ecommerce_app/utils/color_utils.dart';
 import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: <Widget>[
+          optionsBar2(context),
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            hexStringToColor("5E61F4"),
+            hexStringToColor("9546C4"),
+            Colors.black
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+            child: Padding(padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/NEKOSTORE PNG.png"),
+                UserOptions(context)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
