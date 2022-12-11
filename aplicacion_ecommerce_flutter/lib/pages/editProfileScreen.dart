c54ce9cb9193dd6267059f9bd21d61e24e34e29e
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-12-11 03:20:52 -0300

    Create editProfileScreen.dart

---
+++ aplicacion_ecommerce_flutter/lib/pages/editProfileScreen.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce_app/reusable_widgets/reusable_widget.dart';
+import 'package:ecommerce_app/utils/color_utils.dart';
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({Key? key}) : super(key: key);
+
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  TextEditingController _usernameTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: <Widget>[
+          optionsBar2(context),
+        ],
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            hexStringToColor("5E61F4"),
+            hexStringToColor("9546C4"),
+            Colors.black
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+          child: SingleChildScrollView(
+            child: Padding(padding: EdgeInsets.fromLTRB(20, 120, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/NEKOSTORE PNG.png"),
+                UserDataModifier(context, _usernameTextController, _passwordTextController)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
