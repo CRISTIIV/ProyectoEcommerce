b492cea7f62f889abd8a79c82972b1ea76a08c76
CRISTIIV <vicente.cristi@alumnos.ucn.cl>
2022-12-12 14:34:53 -0300

    Create index.dart

---
+++ neko_store_e_commerce/lib/index.dart
@@ -0,0 +1,35 @@
+// Export pages
+export 'login/login_widget.dart' show LoginWidget;
+export 'create_account/create_account_widget.dart' show CreateAccountWidget;
+export 'home_page_m_a_i_n/home_page_m_a_i_n_widget.dart'
+    show HomePageMAINWidget;
+export 'my_orders/my_orders_widget.dart' show MyOrdersWidget;
+export 'product_details/product_details_widget.dart' show ProductDetailsWidget;
+export 'order_details/order_details_widget.dart' show OrderDetailsWidget;
+export 'search_products/search_products_widget.dart' show SearchProductsWidget;
+export 'reviews/reviews_widget.dart' show ReviewsWidget;
+export 'search_result/search_result_widget.dart' show SearchResultWidget;
+export 'profile_page/profile_page_widget.dart' show ProfilePageWidget;
+export 'edit_profile/edit_profile_widget.dart' show EditProfileWidget;
+export 'change_password/change_password_widget.dart' show ChangePasswordWidget;
+export 'create_property_3/create_property3_widget.dart'
+    show CreateProperty3Widget;
+export 'create_property_2/create_property2_widget.dart'
+    show CreateProperty2Widget;
+export 'create_property_1/create_property1_widget.dart'
+    show CreateProperty1Widget;
+export 'edit_property3/edit_property3_widget.dart' show EditProperty3Widget;
+export 'edit_property1/edit_property1_widget.dart' show EditProperty1Widget;
+export 'edit_property2/edit_property2_widget.dart' show EditProperty2Widget;
+export 'admin_page/admin_page_widget.dart' show AdminPageWidget;
+export 'admin_busqueda_modificar/admin_busqueda_modificar_widget.dart'
+    show AdminBusquedaModificarWidget;
+export 'buscar_user/buscar_user_widget.dart' show BuscarUserWidget;
+export 'crear_usuario_admin/crear_usuario_admin_widget.dart'
+    show CrearUsuarioAdminWidget;
+export 'admin_user1/admin_user1_widget.dart' show AdminUser1Widget;
+export 'admin_user2/admin_user2_widget.dart' show AdminUser2Widget;
+export 'admin_user3/admin_user3_widget.dart' show AdminUser3Widget;
+export 'admin_user4/admin_user4_widget.dart' show AdminUser4Widget;
+export 'search_result_admin/search_result_admin_widget.dart'
+    show SearchResultAdminWidget;
